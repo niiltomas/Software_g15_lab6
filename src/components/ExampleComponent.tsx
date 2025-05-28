36265a2c1c01f4fa7b01f0dc2752d202bd69b2ea
zlooper23 <noel.tuset@gmail.com>
2025-05-28 15:16:53 +0200

    Initial setup

---
+++ src/components/ExampleComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExampleComponent: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello from ExampleComponent!</h1>
+            <p>This is an example component in a React application.</p>
+        </div>
+    );
+};
+
+export default ExampleComponent;
